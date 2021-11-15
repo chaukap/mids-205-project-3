5d2e9b1a0eafc833e92d6eac65ffcb110e314f12
HASSAN SAAD <hassansaadca@berkeley.edu>
2021-11-15 11:45:14 -0800

    added test file for testing flask app

---
+++ flask_test.py
@@ -0,0 +1,36 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+start = 1
+
+@app.route('/')
+def hello():
+    return 'THIS IS THE HOME PAGE!'
+
+
+@app.route('/create_file')
+def create_a_file():
+    with open('this_is_a_test.txt', 'w') as f:
+        f.write(f'Creating file and writing first line!')
+        f.close()
+    return 'YOU JUST CREATED A TEXT FILE AND WROTE THE FIRST LINE\n'
+
+
+@app.route('/add_to_file')
+def add_a_line():
+    
+    global start
+    
+    start+=1
+    
+    with open('this_is_a_test.txt', 'a') as f:
+        f.write(f'Adding a line!')
+        f.close()
+        
+        
+    return f'YOU JUST ADDED LINE {start}\n'
+
+
+# @app.route('/test_page')
+# def print()
--- main.py
+++ main.py
@@ -4,31 +4,47 @@ from numpy import random
 from flask import Flask, request
 app = Flask(__name__)
 
-map = np.array(random.randint(100, size=(10000)) < 20).reshape((100, 100))
+board = np.random.binomial(1, .2, size=10000).reshape(100,100)
+
+
+####EVENTS:
+#hit_mine_event
+#uncover_safe_space_event
+#true_positive_event (flag where there is a mine)
+#false_positive_event (flag where there is no mine)
+
+
+def log_to_kafka(topic, event):
+    event.update(request.headers)
+    producer.send(topic, json.dumps(event).encode())
 
 @app.route('/check', methods=['GET'])
 def check():
     x = int(request.args.get('x'))
     y = int(request.args.get('y'))
-    
-    if(map[x][y]):
+    hit_mine_event = {'event_type':'hit_mine'}
+    uncover_safe_space_event = {'event_type':'uncover_safe_space'}
+
+    if(board[x][y]):
+        log_to_kafka('events', hit_mine_event)
         return "Boom! Game Over."
-    
-    return "Safe!" 
+
+    log_to_kafka('events', uncover_safe_space_event)
+    return "Safe!"
 
 @app.route('/flag', methods=['GET'])
 def flag():
     x = int(request.args.get('x'))
     y = int(request.args.get('y'))
-    
-    if(map[x][y]):
+
+    if(board[x][y]):
         return "That's a bomb alright!"
-    
-    return "Nope! Game Over." 
+
+    return "Nope! Game Over." #should an incorrect flag end the game?
 
 @app.route('/solution', methods=['GET'])
 def solution():
-    return str(map)
+    return str(board)
 
 if __name__ == '__main__':
     app.run()
